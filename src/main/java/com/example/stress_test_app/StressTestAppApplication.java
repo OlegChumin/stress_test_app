04791144e6de5029d2c42dfc5d356282a4a0891e
OlegChumin <tschumin.oleg@gmail.com>
2023-12-04 00:33:10 +0300

    Change logic in calling application context

--- src/main/java/com/example/stress_test_app/StressTestAppApplication.java
+++ src/main/java/com/example/stress_test_app/StressTestAppApplication.java
@@ -25,10 +25,15 @@ public class StressTestAppApplication {
 
         ConfigurableApplicationContext context = SpringApplication.run(StressTestAppApplication.class, args);
 
+
+        // Добавляем ожидание завершения выполнения Spring Boot Admin Server
+        Thread.sleep(10_000); // Подождем 5 секунд (или другое подходящее время)
         long startTime = System.currentTimeMillis();
         StressTestBusinessLogic.stressTest();
         long endTime = System.currentTimeMillis();
         System.out.println("Время вычислений:  " + (endTime - startTime) + "мс");
+        // Добавляем ожидание завершения выполнения Spring Boot Admin Server
+        Thread.sleep(5000); // Подождем 5 секунд (или другое подходящее время)
 
         // Закрываем контекст Spring Boot Admin Server
         context.close();
